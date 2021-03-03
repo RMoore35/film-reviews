30ea2c40434abb132d5c6f2724a01c6676f170e3
Ryan Moore <ryan@Ryans-MacBook-Air.local>
2021-03-02 20:08:06 -0600

    directory changes

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # build.sh - PHP to static file generator
-DEST="./public_html/"
+#DEST="./public_html/"
 
-cp index.php $DEST && cd $DEST && php index.php > index.html && cd ../
-cp reviews.php $DEST && cd $DEST && php reviews.php > reviews.html && cd ../
-cp lists.php $DEST && cd $DEST && php lists.php > lists.html && cd ../
-cp about.php $DEST && cd $DEST && php about.php > about.html && cd ../
+php index.php > index.html
+php reviews.php > reviews.html
+php lists.php > lists.html
+php about.php > about.html
 
 # Build our SASS file
 sass --style compressed style.scss:public_html/style.css
