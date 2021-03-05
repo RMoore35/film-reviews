ba4785304473691906800fbcafd52a719c486426
Ryan Moore <ryan@Ryans-MacBook-Air.local>
2021-03-04 18:26:10 -0600

    style changes

--- blog.php
+++ blog.php
@@ -2,6 +2,6 @@
   <div class="main">
       <h2><b>Blog</b></h2><br>
         <p>This is where I will keep all blog posts. I will try to post something at least a few times
-        every month.</p><br>
+        every month.</p><br><br><br><br><br><br><br><br><br><br><br><br><br>
     </div>
 <?php include 'footer.html' ?>
--- contact.php
+++ contact.php
@@ -5,6 +5,6 @@
         manage. I may add one in the future.</p><br>
         <p>The best way to contact me is via email at <a href="mailto:charmingsparks@protonmail.com">
           charmingsparks@protonmail.com</a></p><br>
-        <p>I appreciate any and all feedback!</p><br>
+        <p>I appreciate any and all feedback!</p><br><br><br><br><br>
     </div>
 <?php include 'footer.html' ?>
