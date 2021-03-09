af235781bf9d60964e849347f42b5190084faf7d
Ryan Moore <ryan@Ryans-MacBook-Air.local>
2021-03-08 21:56:05 -0600

    scrape and style updates

--- blog.php
+++ blog.php
@@ -4,9 +4,8 @@
         <p>This is where I will keep all of my blog posts. I will try to post something at least a few times
         every month.</p><br>
       <div class="card">
-        <h2><a href="/posts/first-post.html">First Blog Entry</a></h2>
+        <h2><a href="/posts/first-post.html">The Film World in 2021</a></h2>
         <h5>March 8th, 2021</h5>
-        <p>The film world in 2021.</p><br><br>
       </div>
   </div>
 <?php include 'footer.html' ?>
