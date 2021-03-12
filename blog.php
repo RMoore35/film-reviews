341f60bf1360a2c266a88c203a2ac7ab4aedd4f8
Ryan Moore <ryan@Ryans-MacBook-Air.local>
2021-03-11 20:32:47 -0600

    minor style changes

--- blog.php
+++ blog.php
@@ -10,11 +10,11 @@
       <div class="card">
         <h2><a href="/posts/second-post.html">Film Lovers Unite</a></h2>
         <h5>March 11th, 2021</h5>
-      </div>
+      </div><br>
       <div class="card">
         <h2><a href="/posts/first-post.html">The Film World in 2021</a></h2>
         <h5>March 8th, 2021</h5>
       </div>
-      <br><br><br><br>
+      <br><br><br>
   </div>
 <?php include 'footer.html' ?>
