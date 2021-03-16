92d4391fc16497d4592226c52519d0888aa1c5de
Ryan Moore <ryan@Ryans-MacBook-Air.local>
2021-03-16 07:27:16 -0500

    added more divs to homepage, still need to add favorite films and tags

--- index.php
+++ index.php
@@ -2,42 +2,55 @@
 <div class="row">
   <div class="main">
     <h1>The place for all things film</h1>
-    <!--
-    <h3><b>Welcome!</b></h3>
-    <p>Here you will find my favorite films, favorite actors/actresses, the movies I want to watch (watchlist),
-      as well as a little about myself.</p>
-    <br><h3><b>Some Thoughts</b></h3>
-    <p>I love watching movies. Ever since I was young, I remember fondly getting lost in an hour or
-    two of a story. I love the creativity, the diversity of thought, and the almost endless possibilities.</p>
-    <p>I am not very picky. I will watch almost anything. There have only been a few films that I haven't
-    been able to finish, for various reasons. I hope you find at least a little bit of joy,
-    heartache, and at the very least, an escape from the present.</p>-->
   </div>
   <div class="side">
     <img src="/images/stockphoto6.webp" >
-    <!--
-    <h3><b>Top Four Films</b></h3>
-    <a href="https://www.themoviedb.org/movie/9361-the-last-of-the-mohicans" target="_blank">The Last of the Mohicans</a>
-    <br><br>
-    <a href="https://www.themoviedb.org/movie/7270-matchstick-men" target="_blank">Matchstick Men</a>
-    <br><br>
-    <a href="https://www.themoviedb.org/movie/97367-the-place-beyond-the-pines" target="_blank">The Place Beyond the Pines</a>
-    <br><br>
-    <a href="https://www.themoviedb.org/movie/642-butch-cassidy-and-the-sundance-kid" target="_blank">Butch Cassidy and the Sundance Kid</a>
-    <br><br>
-    <hr>
-    <h3><b>Tags</b></h3>
-    <p>
-      <button name="button"><a href="/tags/tags.html#action">Action</a></button>
-      <button name="button"><a href="/tags/tags.html#adventure">Adventure</a></button>
-      <button name="button"><a href="/tags/tags.html#comedy">Comedy</a></button>
-      <button name="button"><a href="/tags/tags.html#drama">Drama</a></button>
-      <button name="button"><a href="/tags/tags.html#romance">Romance</a></button>
-      <button name="button"><a href="/tags/tags.html#thriller">Thriller</a></button>
-    </p><br>-->
-
   </div>
 </div>
+<div class="main-2">
+  <b><h1>Welcome to my site!</h1></b>
+    <p>Here you will find my favorite films, favorite actors/actresses,
+      as well as a little about myself.</p>
+    <p>You can find all of the films I want to watch over on my
+      <a href="https://letterboxd.com/RMoore35/watchlist/">Letterboxd</a> profile.</p><br><br>
+  <h1>Some Thoughts</h1>
+    <p>I love watching movies. Ever since I was young, I remember fondly getting lost in an hour or
+      two of a story. I love the creativity, the diversity of thought, and the almost endless possibilities.</p>
+    <p>I am not very picky. I will watch almost anything. There have only been a few films that I haven't
+      been able to finish, for various reasons. I hope you find at least a little bit of joy,
+      heartache, and at the very least, an escape from the present.</p>
+</div>
+<div class="main-3">
+  <h1>Latest Reviews</h1>
+  <p><?php $lines = file('top3.txt'); foreach($lines as $line){echo $line;}?></p><br>
+</div>
+<div class="main-3">
+  <p>*****The rest of my reviews can be found on my
+    <a href="https://letterboxd.com/RMoore35/" target="_blank">Letterboxd profile.</a>*****</p><br>
+</div>
+<?php include 'footer.html' ?>
+
+<!--
+<h3><b>Top Four Films</b></h3>
+<a href="https://www.themoviedb.org/movie/9361-the-last-of-the-mohicans" target="_blank">The Last of the Mohicans</a>
+<br><br>
+<a href="https://www.themoviedb.org/movie/7270-matchstick-men" target="_blank">Matchstick Men</a>
+<br><br>
+<a href="https://www.themoviedb.org/movie/97367-the-place-beyond-the-pines" target="_blank">The Place Beyond the Pines</a>
+<br><br>
+<a href="https://www.themoviedb.org/movie/642-butch-cassidy-and-the-sundance-kid" target="_blank">Butch Cassidy and the Sundance Kid</a>
+<br><br>
+<hr>
+<h3><b>Tags</b></h3>
+<p>
+  <button name="button"><a href="/tags/tags.html#action">Action</a></button>
+  <button name="button"><a href="/tags/tags.html#adventure">Adventure</a></button>
+  <button name="button"><a href="/tags/tags.html#comedy">Comedy</a></button>
+  <button name="button"><a href="/tags/tags.html#drama">Drama</a></button>
+  <button name="button"><a href="/tags/tags.html#romance">Romance</a></button>
+  <button name="button"><a href="/tags/tags.html#thriller">Thriller</a></button>
+</p><br>
+
 <div class="row">
   <div class="main-3">
   </div>
@@ -52,5 +65,4 @@
       <button name="button"><a href="/tags/tags.html#thriller">Thriller</a></button>
     </p>
   </div>
-</div>
-<?php include 'footer.html' ?>
+</div>-->
