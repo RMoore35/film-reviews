2c21bc5e1742c88629acb082d5aee510d7284953
Ryan Moore <ryan@Ryans-MacBook-Air.local>
2021-03-16 07:32:13 -0500

    removed reviews.php and .html, changed build.sh and executable

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 # build.sh - PHP to static file generator
 
 php index.php > index.html
-php reviews.php > reviews.html
 php lists.php > lists.html
 php about.php > about.html
 php blog.php > blog.html
