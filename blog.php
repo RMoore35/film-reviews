a7837ca32e90124c84057efaef13e64885491da9
Ryan Moore <ryan@Ryans-MacBook-Air.local>
2021-03-21 21:09:45 -0500

    added times new roman global font, other minor style sheet changes

--- blog.php
+++ blog.php
@@ -2,7 +2,7 @@
   <div class="main-4">
       <h2><b>Blog</b></h2>
         <p>This is where I will keep all of my blog posts. I will try to post something at least a few times
-        every month.</p><br>
+        every month.</p>
       <!--<div class="card">
         <h2><a href="/posts/XXXXX-post.html">INSERT TITLE</a></h2>
         <h5>INSERT DATE</h5>
@@ -15,6 +15,6 @@
         <h3><a href="/posts/first-post.html">The Film World in 2021</a></h3>
         <h5>March 8th, 2021</h5>
       </div>
-      <br><br><br>
+      <br><br>
   </div>
 <?php include 'footer.html' ?>
